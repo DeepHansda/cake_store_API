116d3856dae1b4e8344744bd5358f5d1a831703f
Deep Hansda <deephansda921@gmail.com>
2022-01-21 18:40:55 +0530

    little changes

--- src/Database/connection.js
+++ src/Database/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 dotenv.config()
-const mongoo_uri =  process.env.MONGO_URL
+const mongoo_uri =process.env.MONGO_URL
 // 'mongodb://localhost:27017/cakee_it'
 const mongo_params ={
     useNewUrlParser:true,
